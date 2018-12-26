17eb52611e9e194fd1eb79e0181e46313127ca84
Benjamin Kim <benjaminykim@berkeley.edu>
2018-12-25 23:08:59 -0800

    added r/nba scraper, .gitignore

---
+++ main.py
@@ -0,0 +1,30 @@
+import praw
+
+reddit = praw.Reddit('nbabot')
+subreddit = reddit.subreddit("nba")
+
+"""
+for submission in subreddit.hot(limit=1):
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("----------------------\n")
+
+    submission.comments.replace_more(limit=0)
+    for comment in submission.comments.list():
+        print(comment.body)
+"""
+scores = []
+
+for submission in subreddit.hot():
+    title = submission.title
+    if "GAME THREAD:" not in title:
+        continue
+    else:
+        print(title)
+        submission.comments.replace_more(limit=0)
+        i = 0
+        for comment in submission.comments.list():
+            i += 1
+            print(comment.body)
+        print(i)
